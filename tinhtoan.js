f269e0ec44bb72bfe59cf6dc160ba7abb812453a
tranquocsang <tranquocsang19977@gmail.com>
2019-06-29 12:25:00 +0700

    baitap

---
+++ tinhtoan.js
@@ -0,0 +1,18 @@
+function cong(){
+    let a = parseFloat(document.getElementById("so1").value)
+    let b = parseFloat(document.getElementById("so2").value)
+
+    alert("kết quả = " +(a + b))
+}function tru() { let a = parseFloat(document.getElementById("so1").value)
+    let b = parseFloat(document.getElementById("so2").value)
+    alert("kết quả = " +(a-b))
+
+}function nhan() { let a = parseFloat(document.getElementById("so1").value)
+    let b = parseFloat(document.getElementById("so2").value)
+    alert("kết quả = " +(a*b))
+}function chia() {
+    let a = parseFloat(document.getElementById("so1").value)
+    let b = parseFloat(document.getElementById("so2").value)
+
+    alert("kết quả = " +(a / b))
+}
